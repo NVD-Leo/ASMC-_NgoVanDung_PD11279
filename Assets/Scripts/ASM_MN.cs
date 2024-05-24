48389b9a2fb63c47bd424bcb90d16efce5baa940
Dung Ngo <NVD-Leo>
2024-05-24 20:06:47 +0700

    update ASM GD1

---
+++ Assets/Scripts/ASM_MN.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
+using UnityEditor.Experimental.GraphView;
+using System;
+
+public class ASM_MN : Singleton<ASM_MN>
+{
+    public List<Region> listRegion = new List<Region>();
+    public List<Players> listPlayer = new List<Players>();
+
+    private void Start()
+    {
+        createRegion();
+    }
+
+    public void createRegion()
+    {
+        listRegion.Add(new Region(0, "VN"));
+        listRegion.Add(new Region(1, "VN1"));
+        listRegion.Add(new Region(2, "VN2"));
+        listRegion.Add(new Region(3, "JS"));
+        listRegion.Add(new Region(4, "VS"));
+        listPlayer.Add(new Players(1234, "Dat", 150, listRegion[0]));
+        listPlayer.Add(new Players(2345, "Khanh", 500, listRegion[1]));
+        listPlayer.Add(new Players(3456, "Dung", 1000, listRegion[2]));
+    }
+
+    public string calculate_rank(int score)
+    {
+        if (score < 100) { return "Dong"; }
+        else if (score < 500) { return "Bac"; }
+        else if (score < 1000) { return "Vang"; }
+        else if(score >= 1000) { return "Kim cuong"; }
+        else
+        return null;
+    }
+
+    public void YC1(int id, string name, int score, Region IDregion)
+    {
+        Players player = new Players(ScoreKeeper.Instance.ID, ScoreKeeper.Instance.userName, ScoreKeeper.Instance.score, listRegion[ScoreKeeper.Instance.IDregion]);
+        listPlayer.Add(player);
+    }
+public void YC2()
+    {
+        foreach ( Players player in listPlayer )
+        {
+            string rank = calculate_rank(player.Score);
+            Debug.Log($"ID: {player.Id}, Name: {player.Name}, Score:{player.Score}, Region: {player.region.Name}, Rank: {rank}");
+        }
+    }
+    public void YC3()
+    {
+        foreach (var player in listPlayer.Where(p => p.Score < ScoreKeeper.Instance.score ))
+        {
+            string rank = calculate_rank(player.Score);
+            Debug.Log($"Id: {player.Id}, Name: {player.Name}, Score: {player.Score}, Region: {player.region.Name}, Rank: {rank}");
+        }
+    }
+    public void YC4()
+    {
+        
+        var player = listPlayer.FirstOrDefault(p => p.Id == ScoreKeeper.Instance.ID);
+        if (player != null)
+        {
+            string rank = calculate_rank(player.Score);
+            Debug.Log($"Id: {player.Id}, Name: {player.Name}, Score: {player.Score}, Region: {player.region.Name}, Rank: {rank}");
+        }
+        else
+        {
+            Debug.Log("Player not found");
+        }
+    }
+    public void YC5()
+    {
+        foreach (var player in listPlayer.OrderByDescending(p => p.Score))
+        {
+            string rank = calculate_rank(player.Score) ;
+            Debug.Log($"Id: {player.Id}, Name: {player.Name}, Score: {player.Score}, Region: {player.region.Name},Rank: {rank}");
+        }
+    }
+    public void YC6()
+    {
+        foreach (var player in listPlayer.OrderBy(p => p.Score).Take(5))
+        {
+            string rank = calculate_rank(player.Score);
+            Debug.Log($"Id: {player.Id}, Name: {player.Name}, Score: {player.Score}, Region: {player.region.Name},Rank: {rank}");
+        }
+    }
+    public void YC7()
+    {
+        Thread thread = new Thread(CalculateAndSaveAverageScoreByRegion);
+        thread.Name = "BXH";
+        thread.Start();
+    }
+    void CalculateAndSaveAverageScoreByRegion()
+    {
+        var regionGroups = listPlayer.GroupBy(p => p.region)
+                                    .Select(g => new
+                                    {
+                                        Region = g.Key,
+                                        AverageScore = g.Average(p => p.Score)
+                                    });
+
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter("bxhRegion.txt"))
+        {
+            foreach (var region in regionGroups)
+            {
+                file.WriteLine($"Region: {region.Region.Name}, Average Score: {region.AverageScore}");
+            }
+        }
+    }
+}
+
+[SerializeField]
+public class Region
+{
+    public int ID;
+    public string Name;
+    public Region(int ID, string Name)
+    {
+        this.ID = ID;
+        this.Name = Name;
+    }
+}
+
+[SerializeField]
+public class Players
+{
+    public int Id;
+    public string Name;
+    public int Score;
+    public Region region;
+    public Players( int Id, string Name, int Score, Region region)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Score = Score;
+        this.region = region;
+
+    }
+}
---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    
+
+    void Start()
+    {
+        scoreText.text = "You Scored:\n" + ScoreKeeper.Instance.GetScore();
+
+        ASM_MN.Instance.YC1(1234, "Dung", 200, ASM_MN.Instance.listRegion[3]);
+        Debug.Log("YC2: Xuất danh sách người chơi");
+        ASM_MN.Instance.YC2();
+        Debug.Log("YC3: Xuất người chơi có score bé hơn Player");
+        ASM_MN.Instance.YC3();
+        Debug.Log("YC4: Tìm player theo Id Player");
+        ASM_MN.Instance.YC4();
+        Debug.Log("YC5: Xuất thông tin người chơi theo thứ tự score giảm dần");
+        ASM_MN.Instance.YC5();
+        Debug.Log("YC6: Xuất 5 người chơi có score thấp nhất");
+        ASM_MN.Instance.YC6();
+        Debug.Log("YC7: Tính score trung bình dựa trên Region và lưu vào tập tin");
+        ASM_MN.Instance.YC7();
+    }
+
+    
+
+
+}
